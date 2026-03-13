a4c17e220fc14f8ec46883b5809ad517dca165e6
ktruong6 <ktruong6@terpmail.umd.edu>
2026-03-13 00:01:40 -0400

    added contact form and favorite course query

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,35 @@ def about():  # put application's code here
 @app.route('/about_css')
 def about_css():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite_course')
+def favorite_course():  # put application's code here
+    subject=request.args.get('subject')
+    course_number=request.args.get('course_number')
+
+    return render_template('favorite-course.html',subject=subject,course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact(): # put application's code here
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+
+        if not first_name or not last_name or not email or not message:
+            return render_template("contact.html", submitted=False)
+
+        return render_template(
+            "contact.html",
+            submitted=True,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            message=message
+        )
+
+    return render_template("contact.html", submitted=False)
 
 if __name__ == '__main__':
     app.run()
